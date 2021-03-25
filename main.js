c94faaa280de6a9647147038762ea5955c6f252e
Helgi Steinarr <HelgiSteinarr@users.noreply.github.com>
2021-03-25 16:58:30 +0000

    Smá fikt
    Bætti við takka til að láta hinn spilara draga og fl.

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
-var pScore = 0;
-var playerElement = document.getElementById("player");
+var humanScore = 0;
+var humanElement = document.getElementById("humanCount");
+var cpuScore = 0;
+var cpuElement = document.getElementById("cpuCount");
 
-function dealCard() 
+class Deck
 {
-    pScore += 1;
-    playerElement.innerHTML = pScore;
-
-    if(pScore > 20){
-        console.log("The oppression of the sith will never return; you have lost!");
-    } else {
-        console.log(pScore);
+    constructor(){
+        this.cardsInDeck = [];
+        // TODO: generate deck cards
+    }
+    drawFromDeck(){
+        // TODO: make this function actually 'draw' from this.cardsInDeck instead of just returning randint
+        return Math.floor(Math.random() * (Math.floor(10) - Math.ceil(1) + 1)) + Math.ceil(1);
     }
 }
+
+// At beginning of each round create a new deck when we code the round/game logic. for now we just create it here
+d = new Deck();
+
+function dealCard(player) 
+{
+    switch(player)
+    {
+        case 0: // when human
+            humanScore += d.drawFromDeck();
+            humanElement.innerHTML = humanScore;
+
+            console.log(humanScore)
+            if(humanScore > 20){
+                humanElement.innerHTML += "- The oppression of the sith will never return; you have lost!";
+            }
+            break
+        case 1: // when cpu
+            cpuScore += d.drawFromDeck();
+            cpuElement.innerHTML = cpuScore;
+
+            console.log(cpuScore);
+            if(cpuScore > 20){
+                cpuElement.innerHTML += "- So this is how liberty dies…with thunderous applause; you have lost!"; // vissi ekki hvaða texti mundi hennta svo setti þetta bara
+            }
+            break
+    }
+
+}
+
 
 
 
