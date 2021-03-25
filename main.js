c4b72fe5000907abfa02b94a4b18b35d170854a5
Helgi Steinarr <HelgiSteinarr@users.noreply.github.com>
2021-03-25 17:49:02 +0000

    meira fikt
    Bætti við stand takka og alert hvor vinnnur þegar báðir hafa "staðið"

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-var humanScore = 0;
-var humanElement = document.getElementById("humanCount");
+var hmnScore = 0;
+var hmnElement = document.getElementById("humanCount");
 var cpuScore = 0;
 var cpuElement = document.getElementById("cpuCount");
 
-class Deck
+hmnDrawBtn = document.getElementById("hmnDrawBtn");
+hmnStandBtn = document.getElementById("hmnStandBtn");
+cpuDrawBtn = document.getElementById("cpuDrawBtn");
+cpuStandBtn = document.getElementById("cpuStandBtn");
+let hmnStanding = false;
+let cpuStanding = false;
+
+class MainDeck 
 {
     constructor(){
         this.cardsInDeck = [];
@@ -15,20 +22,32 @@ class Deck
     }
 }
 
+class SideDeck
+{
+    constructor(){
+        this.cardsInDeck = [];
+    }
+    drawFromDeck(){
+
+    }
+}
+
+
 // At beginning of each round create a new deck when we code the round/game logic. for now we just create it here
-d = new Deck();
+d = new MainDeck();
 
 function dealCard(player) 
 {
     switch(player)
     {
         case 0: // when human
-            humanScore += d.drawFromDeck();
-            humanElement.innerHTML = humanScore;
+            hmnScore += d.drawFromDeck();
+            hmnElement.innerHTML = hmnScore;
 
-            console.log(humanScore)
-            if(humanScore > 20){
-                humanElement.innerHTML += "- The oppression of the sith will never return; you have lost!";
+            console.log(hmnScore)
+            if(hmnScore > 20){
+                hmnElement.innerHTML += "- The oppression of the sith will never return; you have lost!";
+                stand(0);
             }
             break
         case 1: // when cpu
@@ -38,10 +57,35 @@ function dealCard(player)
             console.log(cpuScore);
             if(cpuScore > 20){
                 cpuElement.innerHTML += "- So this is how liberty dies…with thunderous applause; you have lost!"; // vissi ekki hvaða texti mundi hennta svo setti þetta bara
+                stand(1);
             }
             break
     }
 
+}
+
+
+
+function stand(player){
+    switch(player){
+        case 0:  // When human
+            hmnStanding = true;
+            hmnDrawBtn.disabled = true;
+            hmnStandBtn.disabled = true;
+            break;
+        case 1:  // When cpu
+            cpuStanding = true;
+            cpuDrawBtn.disabled = true;
+            cpuStandBtn.disabled = true;
+            break;
+    }
+    if((hmnStanding && cpuStanding) && (hmnScore <= 20 && cpuScore <= 20)){
+        if(hmnScore > cpuScore){
+            alert("Human wins !");
+        }else{
+            alert("CPU wins !");
+        }
+    }
 }
 
 
